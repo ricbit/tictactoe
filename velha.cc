d0ec01d84f667516f6ff0a50f357f2dc5e26f4e0
Ricardo Bittencourt <bluepenguin@gmail.com>
2020-04-24 01:32:15 -0300

    Fix recursion termination

--- velha.cc
+++ velha.cc
@@ -6,13 +6,13 @@ using namespace std;
 
 using code = int;
 
-enum Direction {
-  equal = 0,
-  up = 1,
-  down = 2
+enum class Direction {
+  equal,
+  up,
+  down
 };
 
-vector<Direction> all_directions{
+constexpr initializer_list<Direction> all_directions {
   Direction::equal, 
   Direction::up, 
   Direction::down
@@ -24,9 +24,10 @@ class Geometry {
   Geometry() {
     construct_winning_lines();
   }
-  void fill_terrain(vector<int>& terrain, int pos) {
+  void fill_terrain(vector<Direction>& terrain, int pos) {
     if (pos == D) {
       winning_lines.push_back(terrain);
+      return;
     }
     for (auto dir : all_directions) {
       terrain[pos] = dir;
@@ -34,19 +35,17 @@ class Geometry {
     }
   }
   void construct_winning_lines() {    
-    for (int d = 0; d < D; d++) {
-      vector<int> terrain(D);
-      fill_terrain(terrain, 0);
-    }
+    vector<Direction> terrain(D);
+    fill_terrain(terrain, 0);
   }
-  vector<vector<code>> winning_lines;
+  vector<vector<Direction>> winning_lines;
 };
 
 int main() {
   Geometry<3, 2> geom;
   for (auto line : geom.winning_lines) {
     for (auto elem : line) {
-      cout << elem << " ";
+      cout << static_cast<int>(elem) << " ";
     }
     cout << "\n";
   }
