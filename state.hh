bea89536886b919c94633f262508ef1d96bddcbc
Ricardo Bittencourt <bluepenguin@gmail.com>
2020-05-27 11:01:18 -0300

    New file for TrackingList

--- state.hh
+++ state.hh
@@ -14,49 +14,7 @@
 #include <list>
 #include "semantic.hh"
 #include "boarddata.hh"
-
-template<int N, int D>
-class TrackingList {
- public:
-  TrackingList() {
-    for (Position i = 0_pos; i <= board_size; i++) {
-      tracking_list[i] = make_pair(Position{i + 1}, Position{i - 1});
-    }
-    tracking_list[board_size].first = 0_pos;
-    tracking_list[0_pos].second = board_size;
-  }
-  struct Iterator {
-    TrackingList& tlist;
-    Position pos;
-    bool operator!=(const Iterator& that) {
-      return pos != that.pos;
-    }
-    Position operator*() {
-      return pos;
-    }
-    Iterator& operator++() {
-      pos = tlist.tracking_list[pos].first;
-      return *this;
-    }
-  };
-  Iterator end() {
-    return Iterator{*this, board_size};
-  }
-  Iterator begin() {
-    return Iterator{*this, tracking_list[board_size].first};
-  }
-  void remove(Position pos) {
-    tracking_list[tracking_list[pos].second].first = tracking_list[pos].first;
-    tracking_list[tracking_list[pos].first].second = tracking_list[pos].second;
-    tracking_list[pos].first = pos;
-  }
-  bool check(Position pos) const {
-    return tracking_list[pos].first != pos;
-  }
- private:
-  constexpr static Position board_size = BoardData<N, D>::board_size;
-  sarray<Position, pair<Position, Position>, board_size + 1> tracking_list;
-};
+#include "tracking.hh"
 
 template<int N, int D>
 class State {
