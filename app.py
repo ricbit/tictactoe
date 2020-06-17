1b9d41538bf0a44c1e02da4af896f7f6bc7641e0
Ricardo Bittencourt <bluepenguin@gmail.com>
2020-06-16 22:18:16 -0300

    Flask server working

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def encode_result(value):
   d = {0: "X wins", 1: "O wins", 2: "draw"}
   return d[value]
 
-def print_table(n, d, board, node, x_set, o_set, encode):
+def print_table(n, d, board, node, x_set, o_set, current, encode):
     s = []
     s.append('<table class="border board">')
     children = node.children.keys()
@@ -20,7 +20,7 @@ def print_table(n, d, board, node, x_set, o_set, encode):
           s.append("O")
         elif pos in children:
           s.append('<a href="%s">%d</a><br>(%s)' %
-              ("/%d" % pos,
+              ("/%s%d" % (current, pos),
                node.children[pos].count,
                encode_result(node.children[pos].result)))
         else:
@@ -30,19 +30,21 @@ def print_table(n, d, board, node, x_set, o_set, encode):
     s.append("</table><br><br>")
     return s
 
-def print_board(n, d, board, node):
+def print_board(n, d, board, current, node):
   x_set = set(p for i, p in enumerate(board) if i % 2 == 0)
   o_set = set(p for i, p in enumerate(board) if i % 2 == 1)
   s = ['<p>Result: %s</p>' % encode_result(node.result)]
   if d == 2:
-    s.extend(print_table(n, d, board, node, x_set, o_set, lambda i, j: j * n + i))
+    s.extend(print_table(n, d, board, node, x_set, o_set,
+      current, lambda i, j: j * n + i))
     return "".join(s)
   elif d == 3:
     s.append("<table><tr>")
     for k in range(n):
       s.append('<td class="dim3">')
       s.extend(print_table(
-        n, d, board, node, x_set, o_set, lambda i, j: k * n * n + j * n + i))
+        n, d, board, node, x_set, o_set,
+        current, lambda i, j: k * n * n + j * n + i))
       s.append("</td>")
     s.append("</tr></table>")
     return "".join(s)
@@ -76,16 +78,18 @@ def bottom_html():
   </html>
   """
 
-def print_tree(tree, node, board, max_depth, depth, path):
+def print_tree(tree, node, board, max_depth, depth, current, path):
   s = []
-  s.extend(print_board(tree.n, tree.d, board, node))
-  if node.children and depth < len(path):
+  s.extend(print_board(tree.n, tree.d, board, "".join(current), node))
+  if node.children and path:
     childnode = node.children[path[0]]
     board.append(path[0])
-    s.extend(print_tree(tree, childnode, board, max_depth, depth + 1, path[1:]))
+    s.extend(print_tree(
+      tree, childnode, board, max_depth, depth, current + [str(path[0]) + "/"], path[1:]))
     board.pop()
   return s
 
+tree = reader.read_file("solution.txt")
 app = Flask(__name__)
 @app.route("/<path:subpath>")
 def root(subpath):
@@ -96,11 +100,10 @@ def rootempty():
   return render("")
 
 def render(subpath):
-  tree = reader.read_file("solution.txt")
   path = [int(x) for x in subpath.split("/") if x]
   s = []
   s.append(top_html())
-  s.extend(print_tree(tree, tree.root, [], 5, 0, path))
+  s.extend(print_tree(tree, tree.root, [], 5, 0, [], path))
   s.append(bottom_html())
   return "".join(s)
 
