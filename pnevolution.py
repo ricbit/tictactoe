ddd8df6f62fc67ac215c64755f247183a306a9e0
Ricardo Bittencourt <bluepenguin@gmail.com>
2020-07-30 01:51:48 -0300

    Better graphs on pnevolution.py

--- pnevolution.py
+++ pnevolution.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plot
 
 pn = [x.split() for x in open("pnevolution.txt", "rt").readlines()]
-#plot.plot([int(x[0]) for x in pn], label="proof")
-#plot.plot([int(x[1]) for x in pn], label="disproof")
-plot.plot([int(x[2]) for x in pn], label="depth")
-plot.xlabel("Nodes visited")
-plot.ylabel("Proof number")
-plot.legend()
+fig, (ax1, ax2) = plot.subplots(2)
+ax1.plot([int(x[0]) for x in pn], label="proof")
+ax1.plot([int(x[1]) for x in pn], label="disproof")
+ax1.set(xlabel="Nodes visited", ylabel="Proof number")
+ax1.legend()
+ax2.plot([int(x[2]) for x in pn], label="depth")
+ax2.set(xlabel="Nodes visited", ylabel="Depth")
 plot.show()
